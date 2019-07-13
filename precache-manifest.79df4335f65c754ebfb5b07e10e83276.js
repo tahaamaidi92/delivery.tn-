cf6ea74e8ea915a8ec3e184625e92785a5f31268
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-07-13 01:20:44 +0100

    Updates

---
+++ precache-manifest.79df4335f65c754ebfb5b07e10e83276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5715cf8c8f1fbbe8003967adbb65daf9",
+    "url": "/delivery.tn-/index.html"
+  },
+  {
+    "revision": "65d2248439ebe33aa80a",
+    "url": "/delivery.tn-/static/css/main.a22b485b.chunk.css"
+  },
+  {
+    "revision": "c0c06d6761a654170094",
+    "url": "/delivery.tn-/static/js/2.966a546d.chunk.js"
+  },
+  {
+    "revision": "65d2248439ebe33aa80a",
+    "url": "/delivery.tn-/static/js/main.9be5d951.chunk.js"
+  },
+  {
+    "revision": "94a2ade5fe2a35546e4d",
+    "url": "/delivery.tn-/static/js/runtime~main.e36acfe2.js"
+  }
+]);
